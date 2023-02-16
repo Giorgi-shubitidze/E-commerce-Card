1c0abc3bc42c8c48963435038aacd418aaabcec6
Giorgi Shubitidze <giorgishubitidze2066@gmail.com>
2023-02-16 02:49:54 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+var st1 = document.getElementById("star1")
+var st2 = document.getElementById("star2")
+var st3 = document.getElementById("star3")
+var st4 = document.getElementById("star4")
+var st5 = document.getElementById("star5")
+
+
+function sta1() {
+    if (sta1) {
+    st1.src = "images/starF.png"
+    st2.src = "images/star.png"
+    st3.src = "images/star.png"
+    st4.src = "images/star.png"
+    st5.src = "images/star.png"
+}
+}
+
+
+function sta2() {
+    st1.src = "images/starF.png"
+    st2.src = "images/starF.png"
+    if (sta2) {
+        st1.src = "images/starF.png"
+        st2.src = "images/starF.png"
+        st3.src = "images/star.png"
+        st4.src = "images/star.png"
+        st5.src = "images/star.png"
+    }
+}
+
+
+function sta3() {
+    st1.src = "images/starF.png"
+    st2.src = "images/starF.png"
+    st3.src = "images/starF.png"
+    if (sta3) {
+        st1.src = "images/starF.png"
+        st2.src = "images/starF.png"
+        st3.src = "images/starF.png"
+        st4.src = "images/star.png"
+        st5.src = "images/star.png"
+    }
+}
+
+function sta4() {
+    st1.src = "images/starF.png"
+    st2.src = "images/starF.png"
+    st3.src = "images/starF.png"
+    st4.src = "images/starF.png"
+    if (sta4) {
+        st1.src = "images/starF.png"
+        st2.src = "images/starF.png"
+        st3.src = "images/starF.png"
+        st4.src = "images/starF.png"
+        st5.src = "images/star.png"
+    }
+}
+
+
+function sta5() {
+    st1.src = "images/starF.png"
+    st2.src = "images/starF.png"
+    st3.src = "images/starF.png"
+    st4.src = "images/starF.png"
+    st5.src = "images/starF.png"
+}
+
